1440f7c1b81e37943ce42d3b38fad33ae2f46bb4
James Peach <mbax3jp2@student.manchester.ac.uk>
2013-11-28 13:35:16 +0000

    finished ex8

--- COMP16121/ex8/task13.2/HelloWorld.java
+++ COMP16121/ex8/task13.2/HelloWorld.java
@@ -2,6 +2,10 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 public class HelloWorld extends JFrame
 {
+  /**
+   * creates the hello world gui application and shows it
+   * @return a reference to the object
+   */
   public  HelloWorld(){
     super("HelloWorld");
     add(new JLabel("Bonjour tout le monde!"));
@@ -9,8 +13,14 @@ public class HelloWorld extends JFrame
     setVisible(true);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
   }
+
+  /**
+   * starts the application
+   * @param args
+   */
   public static void main(String[] args)
   {
+    new HelloWorld();
     new HelloWorld();
   }
 }
