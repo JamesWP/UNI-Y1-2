d5ddc4ac51f962233d537f5e0b590d1fbe151372
James Peach <james.peach@student.manchester.ac.uk>
2013-11-28 13:06:59 +0000

    finished 13.2

--- COMP16121/ex8/task13.2/HelloWorld.java
+++ COMP16121/ex8/task13.2/HelloWorld.java
@@ -1,7 +1,16 @@
-public class HelloWorld
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+public class HelloWorld extends JFrame
 {
+  public  HelloWorld(){
+    super("HelloWorld");
+    add(new JLabel("Bonjour tout le monde!"));
+    pack();
+    setVisible(true);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+  }
   public static void main(String[] args)
   {
-    System.out.println("Hello World: HelloWorld");
+    new HelloWorld();
   }
 }
