819279259ab4ee045a19d787a2830e87e5a6d23b
James Peach <mbax3jp2@student.manchester.ac.uk>
2013-12-05 09:08:26 +0000

    added ex9 template

---
+++ COMP16121/ex9/task13.12/ThreeWeights.java
@@ -0,0 +1,7 @@
+public class ThreeWeights
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World: ThreeWeights");
+  }
+}
---
+++ COMP16121/ex9/task13.8/MyMath.java
@@ -0,0 +1,7 @@
+public class MyMath
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World: MyMath");
+  }
+}
