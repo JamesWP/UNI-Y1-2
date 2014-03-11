b93744083585226b41515d7a13b951887b9b9446
JamesWP <james_peach01@hotmail.co.uk>
2014-03-11 11:09:40 +0000

    finished point one

--- COMP14112/ex2/naivebayes/YesNoClassifier.java
+++ COMP14112/ex2/naivebayes/YesNoClassifier.java
@@ -10,24 +10,36 @@ public class YesNoClassifier {
 
     public static void main(String[] args)
     {
-	// Read in MFCC data 
-	
-	String mfccDataDirectory = "data/yesno/mfcc/"; 
-	Data yesData = new Data (mfccDataDirectory+"yes");
-	Data noData = new Data (mfccDataDirectory+"no");
+      // Read in MFCC data 
+      
+      String mfccDataDirectory = "data/yesno/mfcc/"; 
+      Data yesData = new Data (mfccDataDirectory+"yes");
+      Data noData = new Data (mfccDataDirectory+"no");
 
-	// Build a naive Bayes classifier
+      // Build a naive Bayes classifier
 
-	Classifier classifier = new Classifier(yesData.getMeanMfcc(),noData.getMeanMfcc(),0.5);
+      Classifier classifier = new Classifier(yesData.getMeanMfcc(),noData.getMeanMfcc(),0.5);
 
-	// Compute the probability of being in class one for the first yes example
-	// using the 1st time-averaged MFCC as the feature
+      // Compute the probability of being in class one for the first yes example
+      // using the 1st time-averaged MFCC as the feature
 
-	int featureNumber = 0; // Using this MFCC component (0 is 1st component)
-	int exampleNumber = 0; // Classifying this example
+      int featureNumber = 0; // Using this MFCC component (0 is 1st component)
 
-	double answer = classifier.classify(yesData.getMeanMfcc(exampleNumber),featureNumber);
-		
-	System.out.println("The probability of this example being a yes is "+Float.toString((float) answer));
+      int totalSamples = yesData.getNumberExamples();
+      totalSamples += noData.getNumberExamples();
+
+      int totalIncorect = 0;
+
+      for (int i=0; i<yesData.getNumberExamples();i++)
+        if (classifier.classify(yesData.getMeanMfcc(i),featureNumber)<0.5)
+          totalIncorect++;
+        
+      for (int i=0; i<noData.getNumberExamples();i++)
+        if (classifier.classify(noData.getMeanMfcc(i),featureNumber)>0.5)
+          totalIncorect++;
+
+      System.out.println("The percentage of incorrect classifications is "
+        +Float.toString((float) totalIncorect / totalSamples * 100));
+      System.out.println("with " + totalIncorect + " incorrect out of " + totalSamples);
     }
 }
