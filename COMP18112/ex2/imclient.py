01015d509c98e401cf20bbcbdbb0dbfcf0a4b8cc
James Peach <james.peach@student.manchester.ac.uk>
2014-03-12 11:30:26 +0000

    python lab n stuff

--- COMP18112/ex2/imclient.py
+++ COMP18112/ex2/imclient.py
@@ -1,32 +1,42 @@
 import uuid
 import im
-from multiprocessing import Process
+from threading import Thread
+import sys
+from time import sleep
 
 server = im.IMServerProxy('http://webdev.cs.manchester.ac.uk/~mbax3jp2/IMServer.php')
 
 #last message id
-#lmid = str(uuid.uuid1())
+lmid = server['lmid']
+
 
 #server['message'] = 'new user logged in'
 #server['lmid'] = lmid
 
 def messageReceiver():
-  lmid = str(uuid.uuid1())
-  while True:
-    while server['lmid']==lmid: pass;
-    print "\nmessage<", server['message'], "\nmessage>",
-    lmid = server['lmid']
+  try:
+    global lmid
+    while True:
+      while server['lmid']==lmid: sleep(1);
+      print
+      print server['message']
+      lmid = server['lmid']
+  except KeyboardInterrupt:
+    raise SystemExit 
 
 def messageSender():
-  lmid = str(uuid.uuid1())
-  while True:
-    server['message'] = message = raw_input('message> ')
-    if message == 'exit': break;
-    server['lmid'] = lmid = str(uuid.uuid1())
+  try:
+    global lmid
+    while True:
+      message = raw_input()
+      if message == 'exit': break;
+      server['message'] = message
+      server['lmid'] = lmid = str(uuid.uuid1())
+  except (KeyboardInterrupt, EOFError):
+    print
+    raise SystemExit
 
-try:
-  Process(target=messageReceiver).start()
-  #Process(target=messageSender).start()
-  messageSender()
-except KeyboardInterrupt:
-  raise SystemExit
+if len(sys.argv) == 1 or sys.argv[1] == 'recv':
+  Thread(target=messageReceiver).start()
+if len(sys.argv) == 1 or sys.argv[1] == 'send':
+  Thread(target=messageSender).start()
