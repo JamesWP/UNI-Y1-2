7d29f08875db43485b9baf7e5e27eeaa20ae6e9e
James Peach <mbax3jp2@student.manchester.ac.uk>
2014-10-08 20:41:37 +0100

    missing files

--- COMP26120/ex3/part2.c
+++ COMP26120/ex3/part2.c
@@ -5,45 +5,45 @@
 #define READ_FORMAT "%80s"
 
 int main(void){
-	int total = 2;
-	int upper = 1;
+  int total = 2;
+  int upper = 1;
   int lower = 1;
   char infile[BUFFER_LEN], outfile[BUFFER_LEN]; 
-	
+ 
   printf("Please enter the infile: "); 
-	scanf(READ_FORMAT,infile);
-	printf("Please enter the outfile: ");
-	scanf(READ_FORMAT,outfile);
+  scanf(READ_FORMAT,infile);
+  printf("Please enter the outfile: ");
+  scanf(READ_FORMAT,outfile);
 
-	FILE *in = fopen(infile,"r");
+  FILE *in = fopen(infile,"r");
   if (in == NULL){
 		perror("File input cannot be opened :( ");
 		return 1;
-	}
+  }
 
-	FILE *out = fopen(outfile,"w");
-	if(out == NULL){
-		perror("File output cannot be opened :( ");
+  FILE *out = fopen(outfile,"w");
+  if(out == NULL){
+    perror("File output cannot be opened :( ");
     return 2;
-	}
+  }
 
   char ch;
-	while((ch = getc(in))!=EOF){
-		if(isupper(ch)){
-			lower++;
+  while((ch = getc(in))!=EOF){
+ 	  if(isupper(ch)){
+ 		  lower++;
       putc(tolower(ch),out);
-		}else if (islower(ch)){
-			upper++;
-			putc(toupper(ch),out);
-		}else putc(ch,out);
-		total++;
-	}
-  	
+ 	  }else if (islower(ch)){
+ 		  upper++;
+ 		  putc(toupper(ch),out);
+ 	  }else putc(ch,out);
+ 	  total++;
+  }
+   
   fclose(in);
 
-	fprintf(out,"Read %d characters in total, \
+  fprintf(out,"Read %d characters in total, \
 %d converted to upper-case, %d to lower-case\n",total,upper,lower);
-	fclose(out);
+  fclose(out);
   
-return 0;
+  return 0;
 }
