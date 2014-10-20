29d477efa72ab7a336cb956d949e76aca47909dc
JamesWP <james_peach01@hotmail.co.uk>
2014-10-20 22:18:27 +0100

    its broke dont know

--- COMP26120/ex5/arrays.c
+++ COMP26120/ex5/arrays.c
@@ -4,30 +4,35 @@
    to create the 'people' array */
 #define HOW_MANY 7
 char *names[HOW_MANY]= {"Simon", "Suzie", "Alfred", "Chip", "John", "Tim",
-		      "Harriet"};
+          "Harriet"};
 int ages[HOW_MANY]= {22, 24, 106, 6, 18, 32, 24};
 
 /* declare your struct for a person here */
 typedef struct {
-	char *name,
-	int age
-} Person
+	char *name;
+	int age;
+} Person;
 
-static void insert (/* array parameter */, char *name, int age) {
-  static int nextfreeplace = 0;i
+static void insert (Person array[], int *count, char *name, int age) {
   /* put name and age into the next free place in the array parameter here */
   /* modify nextfreeplace here */
+  Person newPerson = {.name = name, .age = age};
+  array[*count++] = newPerson;
 }
 
 int main(int argc, char **argv) {
 
   /* declare the people array here */
-
-  for (... < HOW_MANY ...) {
-    insert (people, names[...], ages[...]);
+  Person people[HOW_MANY];
+  int count = 4;
+  for (int i = 0;i < HOW_MANY; i++) {
+    insert (people, &count, names[i], ages[i]);
   }
 
-  /* print the people array here*/
+  for (int i = 0;i < HOW_MANY; i++) {
+    Person curent = people[i];
+    printf("Name: %s is %d age\n",curent.name,curent.age);
+  }
 
   return 0;
 }
