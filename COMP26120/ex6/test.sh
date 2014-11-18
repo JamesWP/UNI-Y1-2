f6ff222f53368227e89da949cb6cc3299ab804c4
James Peach <mbax3jp2@student.manchester.ac.uk>
2014-11-18 11:37:47 +0000

    ex6 starting point

--- COMP26120/ex6/test.sh
+++ COMP26120/ex6/test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 rm results
 echo 1 >>results
-(time part1 1 data1000) 2>>results 
+(time part2 1 data10m) 2>>results 
 echo 10 >>results
-(time part1 10 data1000) 2>>results 
+(time part2 10 data10m) 2>>results 
 echo 100 >>results
-(time part1 100 data1000) 2>>results 
+(time part2 100 data10m) 2>>results 
 echo 1000 >>results
-(time part1 1000 data1000) 2>>results 
+(time part2 1000 data10m) 2>>results 
 echo 10000 >>results
-(time part1 10000 data10k) 2>>results 
+(time part2 10000 data10m) 2>>results 
 echo 100000 >>results
-(time part1 100000 data100k) 2>>results 
+(time part2 100000 data10m) 2>>results 
 echo 1000000 >>results
-(time part1 1000000 data1M) 2>>results
-echo 1000000 >>results
-(time part1 10000000 data10M) 2>> results 
+(time part2 1000000 data10m) 2>>results
+echo 10000000 >>results
+(time part2 10000000 data10m) 2>> results 
