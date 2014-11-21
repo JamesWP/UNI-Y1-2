089833c61a6f422b222f2d98d5cf952022014647
JamesWP <james_peach01@hotmail.co.uk>
2014-11-21 00:12:29 +0000

    finished part1

--- COMP26120/ex8/lists.c
+++ COMP26120/ex8/lists.c
@@ -9,14 +9,20 @@ char *names[HOW_MANY]= {"Simon", "Suzie", "Alfred", "Chip", "John", "Tim",
 int ages[HOW_MANY]= {22, 24, 106, 6, 18, 32, 24};
 
 /* declare your struct for a person here */
-typedef struct {
+typedef struct person {
 	char *name;
 	int age;
+  struct person *next;
 } Person;
 
-void insert (Person *array[], int *count, char *name, int age) {
-  /* put name and age into the next free place in the array parameter here */
-  /* modify nextfreeplace here */
+/**
+ * insert. inserts a person into the head of the list
+ * params
+ * list - a pointer to the first item pointer
+ * name - the person name
+ * age - the person age
+ */
+void insert (Person** list, char *name, int age) {
   Person* newPerson =  (Person *)malloc(sizeof(Person));
   if(newPerson == NULL) {
     fprintf(stderr,"Malloc failed");
@@ -24,26 +30,42 @@ void insert (Person *array[], int *count, char *name, int age) {
   }
   newPerson->name = name;
   newPerson->age = age;
-  array[(*count)++] = newPerson;
+  newPerson->next = *list;
+  *list = newPerson;
+}
+
+/**
+ * printList
+ * prints all the list items in the list pointed to by list
+ */
+void printList(Person* list){
+  while (list!=NULL){
+    Person* tmp = list;
+    list = list->next;
+    printf("Person(name:%s,age:%d)\n",tmp->name,tmp->age);
+  }
+}
+/**
+ * freeList
+ * frees all the list items in the list pointed to by list
+ */
+void freeList(Person* list){
+  while (list!=NULL){
+    Person* tmp = list;
+    list = list->next;
+    free(tmp);
+  }
 }
 
 int main(int argc, char **argv) {
-
-  /* declare the people array here */
-  Person *people[HOW_MANY];
+  Person *people = NULL;
   int count = 0;
   for (int i = 0;i < HOW_MANY; i++) {
-    insert (people, &count, names[i], ages[i]);
+    insert(&people, names[i], ages[i]);
   }
-
-  for (int i = 0;i < HOW_MANY; i++) {
-    Person* curent = people[i];
-    printf("Name: %s is %d age\n",curent->name,curent->age);
-  }
-
-  for (int i = 0;i < HOW_MANY; i++) {
-    free(people[i]);
-  }
+  
+  printList(people);
+  freeList(people);
 
   return 0;
 }
