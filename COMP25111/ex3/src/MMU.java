b8aa3436e6955287b5f8dc6fe70cddfe1284f5ad
JamesWP <james_peach01@hotmail.co.uk>
2014-12-08 14:26:21 +0000

    finished ex3

--- COMP25111/ex3/src/MMU.java
+++ COMP25111/ex3/src/MMU.java
@@ -3,9 +3,9 @@ class Page_Table_Entry
   public boolean resident;
   public boolean used;
   public boolean dirty;
-
-  public int page;
+  public int page_frame;
   public int last_used;
+  public int time_in;
 
   public Page_Table_Entry()
   {
@@ -17,7 +17,7 @@ class Page_Table_Entry
 public class MMU
 {
 
-  int page_faults = 0, rejected_pages = 0, writebacks = 0;
+  int page_faults = 0, rejected_pages = 0, writebacks = 0,page_frame_high =0;
 
   final int no_pages, no_page_frames;
 
@@ -27,97 +27,100 @@ public class MMU
   {
     this.no_pages = no_pages;
     this.no_page_frames = no_page_frames;
-    this.page_table = new Page_Table_Entry[no_page_frames];
-    for (int i = 0; i < no_page_frames; i++)
+    this.page_table = new Page_Table_Entry[no_pages];
+    for (int i = 0; i < no_pages; i++)
       page_table[i] = new Page_Table_Entry();
   }
 
   public int memRead(int addr, int timestamp)
   {
-    int page_offset = pageOffset(addr);
     int page_frame_no = getOrLoadPageFrame(addr, timestamp);
     if (page_frame_no == -1) return -1;
-    return pageFrameMemAddr(page_frame_no, page_offset);
+    return page_frame_no;
   }
 
   public void memWrite(int addr, int timestamp)
   {
-    int page_offset = pageOffset(addr);
     int page_frame_no = getOrLoadPageFrame(addr, timestamp);
     if (page_frame_no == -1) return;
-    page_table[page_frame_no].dirty = true;
+
+    System.out.println("addr" + addr + " allocated" + page_frame_no);
+    page_table[addr].dirty = true;
   }
 
   public int getOrLoadPageFrame(int addr, int timestamp)
   {
-    int page_no = pageNo(addr);
-    int page_offset = pageOffset(addr);
-
-    if (page_no > no_pages)
+    if (addr > no_pages)
     {
       rejected_pages++;
       return -1;
     }
 
-    for (int page_frame_no = 0; page_frame_no < no_page_frames; page_frame_no++)
+
+    if (page_table[addr].resident)
     {
-      if (page_table[page_frame_no].page == page_no
-              && page_table[page_frame_no].resident)
-      {
-        // found
-        page_table[page_frame_no].last_used = timestamp;
-        return page_frame_no;
-      }
+      page_table[addr].last_used = timestamp;
+      return page_table[addr].page_frame;
     }
 
     page_faults++;
+
+
     //  miss not in table
-    int page_frame_no = getLRUPageFrameNo();
+    int page_frame_no;
 
-    if (page_table[page_frame_no].resident)
-      rejected_pages++;
-
-    // if dirty write data back
-    if (page_table[page_frame_no].dirty)
-    {
-      writebacks++;
+    if (page_frame_high<no_page_frames){
+      page_frame_no = page_frame_high++;
+    }else{
+      page_frame_no = getLRUPageFrameNo();
+      //page_frame_no = getFIFOPageFrameNo();
     }
-    // swap out new page
-    page_table[page_frame_no].page = page_no;
-    page_table[page_frame_no].resident = true;
-    page_table[page_frame_no].dirty = false;
-    page_table[page_frame_no].last_used = timestamp;
+
+
+    for(int i = 0;i<no_pages;i++)
+      if(page_table[i].resident && page_table[i].page_frame==page_frame_no){
+        if(page_table[i].dirty) writebacks++;
+        page_table[i].dirty = false;
+        page_table[i].resident = false;
+      }
+
+    page_table[addr].page_frame = page_frame_no;
+    page_table[addr].resident = true;
+    page_table[addr].dirty = false;
+    page_table[addr].last_used = timestamp;
+    page_table[addr].time_in = timestamp;
 
     return page_frame_no;
   }
 
-  public int pageNo(int addr)
-  {
-    return (addr >> 4) & 0xffff;
-  }
-
-  public int pageOffset(int addr)
-  {
-    return addr & 0xffff;
-  }
-
-  public int pageFrameMemAddr(int pageFrame, int offset)
-  {
-    return (pageFrame << 4) | (offset & 0xffff);
-  }
-
   public int getLRUPageFrameNo()
   {
-    int oldestTime = page_table[0].last_used;
+
+    int oldestTime = 0x7FFFFF;
     int oldestFrame = 0;
-    for (int i = 0; i < no_page_frames; i++)
+    for (int i = 0; i < no_pages; i++)
     {
-      if (oldestTime < page_table[i].last_used)
+      if (oldestTime > page_table[i].last_used && page_table[i].resident)
       {
         oldestTime = page_table[i].last_used;
         oldestFrame = i;
       }
     }
-    return oldestFrame;
+    return page_table[oldestFrame].page_frame;
+  }
+
+  public int getFIFOPageFrameNo()
+  {
+    int oldestTime = 0x7FFFFF;
+    int oldestFrame = 0;
+    for (int i = 0; i < no_pages; i++)
+    {
+      if (oldestTime > page_table[i].time_in && page_table[i].resident)
+      {
+        oldestTime = page_table[i].time_in;
+        oldestFrame = i;
+      }
+    }
+    return page_table[oldestFrame].page_frame;
   }
 }
