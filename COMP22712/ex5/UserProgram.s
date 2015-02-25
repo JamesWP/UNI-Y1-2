2111a131e689c293e77972323750e2dbff38a200
James Peach <mbax3jp2@student.manchester.ac.uk>
2015-02-25 10:57:36 +0000

    finisehd ex5

--- COMP22712/ex5/UserProgram.s
+++ COMP22712/ex5/UserProgram.s
@@ -6,7 +6,7 @@
 
 ;------------------------
 ;-- literals
-TIMER_MAX_VAL   EQU     0xFF
+TIMER_MAX_VAL   EQU     256
 
 ;------------------------
 ;-- variables memory
@@ -15,59 +15,63 @@ my_counter                ; number of 100ms passed scince bootup
 
 ;------------------------
 ;- UserProgram
+;- 
+;-  Psudo code: 
+;-     read timer value
+;-     calculate difference from last read
+;-     accumulate difference in counter
+;-     if difference > 0 
+;-       update display
+;-       if countdown is reached
+;-         reset countodwn and timer to 0
+;-       if countdown is active
+;-         if button still pressed: continue
+;-         else reset countdown to 0
+;-       else if reset button is pressed
+;-         set countdown = gettime + 0xFF000
+;-      
+
 Main    
         ; init timer
         MOV   r10, #0           ; r10 is miliseconds not counted ; prev remainder
         MOV   r9 , #0           ; last value of counter
         MOV   r8 , #0           ; reset countdown
+        STR   r8 , my_counter
 
         ADRL  r0 , MESSAGE
-        SVC   2
+        SVC   SVC_STR
 1
         ; load curent counter
-        SVC   3
+        SVC   SVC_TIME
         ; find difference
         
         ; if new < old
         CMP   r0, r9
-        ;  time passed is ( 255 - (old - new) )
-        SUBLS r1, r9, r0
-        RSBLS r1, r1, #TIMER_MAX_VAL
+        ;  time passed is ( 256 - (old - new) )
+        SUBLT r1, r9, r0
+        RSBLT r1, r1, #TIMER_MAX_VAL
         ; else 
         ;  time passed is ( new - old )
-        SUBHI r1, r0, r9
+        SUBGE r1, r0, r9
 
         ; save old timer
         MOV   r9, r0
 
-        ;divide time passed by 100 and get quotient: r0 and remainder: r2
-
-        ; first add previous remainder
-        ADD r1, r1, r10
-
-        ; save new remainder
-        MOV   r10, r2
-
-
         ; miliseconds passed is += r0:(time passed / 100)
         LDR   r3, my_counter
-        ADD   r3, r3, r0
+        ADD   r3, r3, r1
         STR   r3, my_counter
 
-        ADD   r7, r7, #1
-        CMP   r7, #ff00
-
-        ; update display every 1000 itterations if not update itteraton 
-        ; carry on 
-        BLO   %b1
+        ; if delta is >0 then display new value else loop
+        CMP   r1, #0
+        BEQ   %b1
 
         MOV   r7, #0
         MOV   r0, r3
         BL    DisplayTimer
 
-
         ; read button state
-2       SVC   5
+2       SVC   SVC_BUTT
         CMP   r0, #1
         BEQ   %b2
 
@@ -81,13 +85,13 @@ Main
         MOVLO r0, #0
         STRLO r0, my_counter
         ; if button not pressed remove countdown
-        SVC   5
+        SVC   SVC_BUTT
         CMP   r0, #2
         MOVNE r8, #0
         B     %b1     ;; repeat timer loop
 3
         ; else if (button pressed)
-        SVC   5
+        SVC   SVC_BUTT
         CMP   r0, #2
         ; store limit 
         ADDEQ r8, r3, #0xFF000
@@ -127,21 +131,20 @@ tenthou DEFW 10000
 hundthou DEFW 100000
 
 DisplayTimer
-        PUSH {r1,r2,r3,LR}
+        PUSH {r0,r1,r2,r3,LR}
 
-        MOV     r1, #1000
-        BL      Divide
-
+        ; pre devide value and get remainder to wrap value if large
         LDR     r1, hundthou
         BL      Divide
         MOV     r3, r2
 
         ; move cursor to beginning of line
-        MOV     r0, #('\r')
-        SVC     1
+        MOV     r0, #CARR_RET
+        SVC     SVC_CHAR
 
         MOV     r0, r3
 
+        ; display values
 
         ;---- tenthou   -----
         ;Divide r0 / 10000
@@ -149,7 +152,7 @@ DisplayTimer
         BL      Divide
         ;Print quotient
         ADD     r0, r0, #('0')
-        SVC     1
+        SVC     SVC_CHAR
 
         ;---- thousands -----
         ;Divide r0 / 1000
@@ -158,11 +161,11 @@ DisplayTimer
         BL      Divide
         ;Print quotient
         ADD     r0, r0, #('0')
-        SVC     1
+        SVC     SVC_CHAR
 
+        ;--- decimal point --
         MOV     r0, #('.')
-        SVC     1
-
+        SVC     SVC_CHAR
 
         ;---- hundreds  -----
         ;Divide r0 / 100
@@ -171,7 +174,7 @@ DisplayTimer
         BL      Divide
         ;Print quotient
         ADD     r0, r0, #('0')
-        SVC     1
+        SVC     SVC_CHAR
         
         ;---- tens      -----
         ;Divide r0 / 10
@@ -180,16 +183,16 @@ DisplayTimer
         BL      Divide
         ;Print quotient
         ADD     r0, r0, #('0')
-        SVC     1
+        SVC     SVC_CHAR
 
         ;---- units -----
         ;print remainder
         MOV     r0, r2
         ADD     r0, r0, #('0')
-        SVC     1
+        SVC     SVC_CHAR
 
-        POP {r1,r2,r3,LR}
+        POP {r0,r1,r2,r3,LR}
         MOV PC, LR
 ;------------------------
 
-MESSAGE DEFB 'Stopwatch v1.0\r\n', 0x0
+MESSAGE DEFB 'Stopwatch v1.1\r\n', 0x0
