889464a970a8b9ea59a6bddce892d0382a1c8119
JamesWP <james_peach01@hotmail.co.uk>
2015-03-19 10:06:30 +0000

    missing files n stuff

--- COMP28112/ex2/BookingJavaClient/src/Client.java
+++ COMP28112/ex2/BookingJavaClient/src/Client.java
@@ -3,26 +3,24 @@ import java.util.List;
 /**
  * Created by james on 20/02/15.
  */
-public class Client
-{
-  public static void main(String[] args){
-    CancellationRequest req = new CancellationRequest(Config.userAccount,27,
-                                                    Config.bandAPI);
-    //ReservationRequest req = new ReservationRequest(Config.userAccount,27,
-    //                                                  Config.bandAPI);
-    //ReservationRequest.RequestResultState resp = req.execute();
-
-    //System.out.println("Client response:" + resp.toString());
-//    AvailabilityRequest req = new AvailabilityRequest(Config.userAccount,
- //                                                     Config.bandAPI);
-    //BookingRequest req = new BookingRequest(Config.userAccount,
-    //                                                 Config.bandAPI);
-    //List<Integer> available= req.executeList();
-
-      Request.RequestResultState result =  req.execute();
-
-      System.out.println(result.toString());
-    //for(int i: available) System.out.printf("Slot: %d, ",i);
-    //System.out.println();
-  }
+public class Client {
+    public static void main(String[] args) {
+        AvailabilityRequest req = new AvailabilityRequest(Config.userAccount, Config.bandAPI);
+        List<Integer> available = req.executeList();
+        for (int i : available) System.out.printf("Slot: %d, ", i);
+    }
 }
+
+//CancellationRequest req = new CancellationRequest(Config.userAccount,27,
+//ReservationRequest req = new ReservationRequest(Config.userAccount,27,
+//                                                  Config.bandAPI);
+//ReservationRequest.RequestResultState resp = req.execute();
+
+//System.out.println("Client response:" + resp.toString());
+//    AvailabilityRequest req = new AvailabilityRequest(Config.userAccount,
+//                                                     Config.bandAPI);
+//BookingRequest req = new BookingRequest(Config.userAccount,
+//                                                 Config.bandAPI);
+//List<Integer> available= req.executeList();
+//for(int i: available) System.out.printf("Slot: %d, ",i);
+//System.out.println();
